a3ea72900edbcaef9ae2fba447ad860dda3e331f
jhpoelen <jhpoelen@xs4all.nl>
2019-08-05 15:36:49 -0700

    fix

--- get-libs.sh
+++ get-libs.sh
@@ -10,7 +10,6 @@ IDIGBIO_SPARK_JAR=lib/idigbio-spark.jar
 
 mkdir -p lib
 
-[ -f $IDIGBIO_SPARK_JAR ] || wget https://github.com/bio-guoda/idigbio-spark/releases/download/0.0.1/iDigBio-LD-assembly-1.5.9.jar -O 
-$IDIGBIO_SPARK_JAR
+[ -f $IDIGBIO_SPARK_JAR ] || wget https://github.com/bio-guoda/idigbio-spark/releases/download/0.0.1/iDigBio-LD-assembly-1.5.9.jar -O $IDIGBIO_SPARK_JAR
 [ -f $PRESTON_JAR ] || wget https://github.com/bio-guoda/preston/releases/download/${PRESTON_VERSION}/preston.jar -O $PRESTON_JAR
  
