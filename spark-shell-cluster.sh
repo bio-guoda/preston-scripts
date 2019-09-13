b44e7180b99b50fb679de42626b699b03281189a
jhpoelen <jhpoelen@xs4all.nl>
2019-09-13 13:41:00 -0700

    remove tailing whitespaces

--- spark-shell-cluster.sh
+++ spark-shell-cluster.sh
@@ -1,12 +1,12 @@
-#!/bin/bash                                                                                                 
-#                                                                                                           
-# Starts a spark-shell in the guoda cluster. 
-# 
-# Please use with care and close when done, because it take up all the resources. 
-#                                                                                                           
-                                                                                                            
-spark-shell \                                                                                               
-  --master mesos://zk://mesos01:2181,mesos02:2181,mesos03:2181/mesos \                                      
-  --driver-memory 4G \                                                                                      
-  --executor-memory 20G \                                                                                   
+#!/bin/bash
+#
+# Starts a spark-shell in the guoda cluster.
+#
+# Please use with care and close when done, because it take up all the resources.
+#
+
+spark-shell \
+  --master mesos://zk://mesos01:2181,mesos02:2181,mesos03:2181/mesos \
+  --driver-memory 4G \
+  --executor-memory 20G \
   --conf spark.sql.caseSensitive=true
