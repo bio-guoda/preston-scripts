a643a2bf3022d12410c8a7e65b8ede693fc6a7ef
jhpoelen <jhpoelen@xs4all.nl>
2019-10-11 09:55:25 -0700

    silence parallel citations

--- stats.sh
+++ stats.sh
@@ -6,12 +6,12 @@
 
 last_versions=6
 
-uniq_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel  preston get {1} | grep "hasVersion" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
-rotten_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel  preston get {1} | grep "hasVersion" | grep  "well-known" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
-drifing_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel  preston get {1} | grep "hasVersion" | grep -v "well-known" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
+uniq_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel --citation  preston get {1} | grep "hasVersion" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
+rotten_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel --citation  preston get {1} | grep "hasVersion" | grep  "well-known" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
+drifing_urls=$(preston history -l tsv | tail -n$last_versions | cut -f1 | parallel --citation  preston get {1} | grep "hasVersion" | grep -v "well-known" | sort | uniq | cut -d ' ' -f1 | sort | uniq -c | sort -nr | wc -l)
 
-start_period=$(preston history -l tsv | tail -n$last_versions | head -n1 | cut -f1 | parallel preston get {1} | head -n10 | grep startedAt)
-end_period=$(preston history -l tsv | tail -n1 | cut -f1 | parallel preston get {1} | head -n10 | grep startedAt)
+start_period=$(preston history -l tsv | tail -n$last_versions | head -n1 | cut -f1 | parallel --citation preston get {1} | head -n10 | grep startedAt)
+end_period=$(preston history -l tsv | tail -n1 | cut -f1 | parallel --citation preston get {1} | head -n10 | grep startedAt)
 
 echo $start_period
 echo $end_period
@@ -21,18 +21,18 @@ echo [$rotten_urls] rotten urls
 echo [$drifing_urls] drifting urls
 
 echo calculating hash sizes
-find data -type f | parallel ls -s --block-size=1 {1} | pv -l | awk -F ' ' '{ print "hash://sha256/" substr($2,12) "\t" $1 }' | sort > hash_sizes.tsv
+find data -type f | parallel --citation ls -s --block-size=1 {1} | pv -l | awk -F ' ' '{ print "hash://sha256/" substr($2,12) "\t" $1 }' | sort > hash_sizes.tsv
 
 echo total hash size 
 cat hash_sizes.tsv | cut -f2 | awk '{ sum+=$1 } END {print sum / (1024 * 1024 * 1024 )}'
 
 echo prov log start times, hashes and activity uuids
-preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel "preston get {1} | grep startedAtTime | head -n1 | sed s+\.$+\<{1}\>.+" > prov-startedAt.nq
+preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel --citation "preston get {1} | grep startedAtTime | head -n1 | sed s+\.$+\<{1}\>.+" > prov-startedAt.nq
 
 cat prov-startedAt.nq | awk -F ' ' '{ print substr($4, 2, 78) "\t" substr($3,2,24) }' | sort > prov-hash-time.tsv
 
 echo create list of data hashes appearing in prov logs 
-preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel "preston get {1} | tr ' ' '\n' | grep "hash://sha256" | sed s+^+{1}+ | tr '<' '\t' | tr -d '>'" | sort | uniq > prov-hash-data-hash.tsv
+preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel --citation "preston get {1} | tr ' ' '\n' | grep "hash://sha256" | sed s+^+{1}+ | tr '<' '\t' | tr -d '>'" | sort | uniq > prov-hash-data-hash.tsv
 
 echo join timed prov hashes with data hashes
 join prov-hash-time.tsv prov-hash-data-hash.tsv > prov-hash-time-data-hash.tsv
@@ -48,6 +48,6 @@ echo -e "data_hash\tdata_size_bytes\tprov_start_time\tprov_hash\tdata_retrieved_
 join data-hash-size-time-prov-hash.tsv data-hash-data-time.tsv | tr ' ' '\t' >> data-hash-size-prov-time-prov-hash-data-time.tsv
 
 echo create a list of urls used in provenance logs
-preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel "preston get {1} | grep -v \"ns#seeAlso\" | tr ' ' '\n' | grep -P \"http([s])*://\" | grep -v 'deeplinker' | grep -v 'purl\.org' | grep -v 'w3\.org' | sed s+^+{1}+ | sort | uniq" | tr '<' '\t' | tr -d '>' | sort | uniq > prov-hash-url.tsv
+preston history -l tsv | tr '\t' '\n' | grep hash | sort | uniq | parallel --citation "preston get {1} | grep -v \"ns#seeAlso\" | tr ' ' '\n' | grep -P \"http([s])*://\" | grep -v 'deeplinker' | grep -v 'purl\.org' | grep -v 'w3\.org' | sed s+^+{1}+ | sort | uniq" | tr '<' '\t' | tr -d '>' | sort | uniq > prov-hash-url.tsv
 
 join prov-hash-time.tsv prov-hash-url.tsv | tr ' ' '\t' > prov-hash-time-url.tsv
