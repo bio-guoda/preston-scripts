db5259a05ac2d70d3a78cb78ce7202997179c64a
jhpoelen <jhpoelen@xs4all.nl>
2019-10-13 17:08:34 -0700

    update readme; include individual provenance index files only; provenance logs available through tar.gz files only

--- package-preston.sh
+++ package-preston.sh
@@ -13,8 +13,7 @@ set -xe
 ## replace start ##
 NETWORK_NAME=DataONE
 PRESTON_NETWORK_SEED="https://dataone.org"
-NETWORK_CITATION="${NETWORK_NAME}, https://www.dataone.org"
-NETWORK_LONGNAME="Data Observation Network for Earth (${NETWORK_NAME}, [1])"
+NETWORK_LONGNAME="Data Observation Network for Earth"
 PRESTON_REMOTE_URL=https://zenodo.org/record/3277312/files
 ## replace end ##
 
@@ -25,7 +24,7 @@ fi
 
 DATE_RANGE_START=$(preston ls -l tsv | grep "prov#generatedAtTime" | cut -f3 | head -n1 | xargs -I {} date --iso-8601 -d {})
 DATE_RANGE_END=$(preston ls -l tsv | grep "prov#generatedAtTime" | cut -f3 | tail -n1 | xargs -I {} date --iso-8601 -d {})
-
+LAST_PROVENANCE_VERSION=$(preston history -l tsv | tr '\t' '\n' | grep "hash:" | tail -n1)
 PRESTON_HISTORY=$(preston history) 
 
 PRESTON_VERIFY_HEAD=$(preston verify | head -n4) 
@@ -33,31 +32,26 @@ PRESTON_VERIFY_HEAD=$(preston verify | head -n4)
 PRESTON_VERSION=$(preston version)
 
 mkdir -p $PWD/tmp/provindex
-mkdir -p $PWD/tmp/prov
 TMPDIR=$(mktemp -d -p $PWD/tmp)
 cp $(which preston) $TMPDIR/preston.jar
 preston cp --type provindex $TMPDIR/provindex
 
 PRESTON_PROV_INDEX=$(ls -1 $TMPDIR/provindex)
 
-preston cp --type prov $TMPDIR/prov
-
-PRESTON_PROV=$(ls -1 $TMPDIR/prov)
-
 envsubst >$TMPDIR/README <<EOF
 A biodiversity dataset graph: ${NETWORK_NAME}
 
-The intended use of this archive is to facilitate meta-analysis of the ${NETWORK_LONGNAME}. ${NETWORK_NAME} is a distributed infrastructure that provides information about earth observation data. 
+The intended use of this archive is to facilitate meta-analysis of the ${NETWORK_LONGNAME} (${NETWORK_NAME}). ${NETWORK_NAME} is a distributed infrastructure that provides information about earth observation data. 
 
-This dataset provides versioned snapshots of the ${NETWORK_NAME} network as tracked by Preston [2] between ${DATE_RANGE_START} and ${DATE_RANGE_END}.  
+This dataset provides versioned snapshots of the ${NETWORK_NAME} network as tracked by Preston [2] between ${DATE_RANGE_START} and ${DATE_RANGE_END} using "preston update -u ${PRESTON_NETWORK_SEED}". 
 
-The archive consists of 256 individual parts (e.g., preston-00.tar.gz, preston-01.tar.gz, ...) to allow for parallel file downloads. The archive contains three types of files: index files, provenance logs and data files. In addition, index files and provenance logs have been individually included in this dataset publication for easy access. Index files provide a way to links provenance files in time to eestablish a versioning mechanism. Provenance files describe how, when and where the ${NETWORK_NAME} meta-data files were retrieved. For more information, please visit https://preston.guoda.bio or https://doi.org/10.5281/zenodo.1410543).  
+The archive consists of 256 individual parts (e.g., preston-00.tar.gz, preston-01.tar.gz, ...) to allow for parallel file downloads. The archive contains three types of files: index files, provenance logs and data files. In addition, index files have been individually included in this dataset publication to facilitate remote access. Index files provide a way to links provenance files in time to eestablish a versioning mechanism. Provenance files describe how, when and where the ${NETWORK_NAME} meta-data files were retrieved. For more information, please visit https://preston.guoda.bio or https://doi.org/10.5281/zenodo.1410543).  
 
 To retrieve and verify the downloaded ${NETWORK_NAME} biodiversity dataset graph, first concatenate all the downloaded preston-*.tar.gz files (e.g., cat preston-*.tar.gz > preston.tar.gz). Then, extract the archives into a "data" folder. Alternatively, you can use the preston[2] command-line tool to "clone" this dataset using:
 
 $ java -jar preston.jar clone --remote ${PRESTON_REMOTE_URL}
 
-After that, verify the index of the archive by reproducing the following result:
+After that, verify the index of the archive by reproducing the following provenance log history:
 
 $ java -jar preston.jar history
 ${PRESTON_HISTORY}
@@ -79,28 +73,25 @@ README
 -- executable java jar containing preston[2] v${PRESTON_VERSION}. --
 preston.jar
 
--- preston archives containing ${NETWORK_NAME} data files, their provenance and a provenance index --
+-- preston archives containing ${NETWORK_NAME} data files, associated provenance logs and a provenance index --
 preston-[00-ff].tar.gz 
 
 -- individual provenance index files --
 ${PRESTON_PROV_INDEX}
-
--- individual provenance logs --
-${PRESTON_PROV}
 
 --- end of file descriptions ---
 
 
 References 
 
-[1] ${NETWORK_CITATION}
-[2] https://preston.guoda.bio, https://doi.org/10.5281/zenodo.1410543 . ${NETWORK_NAME} was crawled via Preston with "preston update -u ${PRESTON_NETWORK_SEED}".
+[1] ${NETWORK_LONGNAME} (${NETWORK_NAME}, ${PRESTON_NETWORK_SEED}) accessed from ${DATE_RANGE_START} to ${DATE_RANGE_END} with provenance ${LAST_PROVENANCE_VERSION}.
+[2] https://preston.guoda.bio, https://doi.org/10.5281/zenodo.1410543 . 
+
 
 This work is funded in part by grant NSF OAC 1839201 from the National Science Foundation
 EOF
 
 mv $TMPDIR/provindex/* $TMPDIR
-mv $TMPDIR/prov/* $TMPDIR
 
 mkdir -p $TMPDIR/old-tars
 mv data/*.tar.gz $TMPDIR/old-tars || true
@@ -111,7 +102,6 @@ ls -1 data/ | grep -P "^[0-9a-f]{2}$" | xargs -I {} sh -c "cd data && tar cvz {}
 mv data/preston*.tar.gz $TMPDIR
 mv $TMPDIR/old-tars/* data/ || true
 rm -rf $TMPDIR/old-tars 
-rm -rf $TMPDIR/prov
 rm -rf $TMPDIR/provindex
 
 ls -1 $TMPDIR
