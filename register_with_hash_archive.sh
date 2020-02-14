fc1e295dadb3a1d8eee8945880e4047283591437
jhpoelen <jhpoelen@xs4all.nl>
2020-02-14 09:24:40 -0800

    add ability to limit versions

--- register_with_hash_archive.sh
+++ register_with_hash_archive.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 # Register all preston urls and associated provenance logs with hash-archive.org
 #
+#
+# usage: 
+#   ./register_with_hash_archive.sh [number of recent provenance versions to register]
+# 
 
-/usr/local/bin/preston history -l tsv | grep Version | cut -f1,3 | tr '\t' '\n' | grep -v "deeplinker\.bio/\.well-known/genid" | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 curl 
+set -x
 
-/usr/local/bin/preston ls -l tsv | grep Version | cut -f1,3 | tr '\t' '\n' | grep -v "deeplinker\.bio/\.well-known/genid" | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 curl 
+if [[ $1 -gt 0 ]]
+then
+  TAIL="tail -n$1"
+else
+  TAIL="cat"
+fi
+
+PROV_HASHES=$(/usr/local/bin/preston history -l tsv | cut -f1,3 | tr '\t' '\n' | grep hash | $TAIL)
+
+echo $PROV_HASHES | tr ' ' '\n' | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 echo curl 
+
+echo $PROV_HASHES | tr ' ' '\n' | xargs -L1 /usr/local/bin/preston cat | grep Version | cut -d' ' -f3 | sed -E $'s/^<//' | sed -E $'s/> .$//' | grep -v "deeplinker\.bio/\.well-known/genid" | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 echo curl 
