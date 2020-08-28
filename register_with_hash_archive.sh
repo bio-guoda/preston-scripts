764b4ec62a7c7eb012ed9e7dd23e86e98a6cb2cb
jhpoelen <jhpoelen@xs4all.nl>
2020-08-28 16:30:42 -0700

    fix bug in hash registration

--- register_with_hash_archive.sh
+++ register_with_hash_archive.sh
@@ -15,8 +15,10 @@ else
   TAIL="cat"
 fi
 
+HOST_URL="https://deeplinker.bio"
+
 PROV_HASHES=$(/usr/local/bin/preston history -l tsv | cut -f1,3 | tr '\t' '\n' | grep hash | $TAIL)
 
-echo $PROV_HASHES | tr ' ' '\n' | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 curl 
+echo $PROV_HASHES | tr ' ' '\n' | sort | uniq | sed -e 's+hash://sha256+https://deeplinker.bio+g' | sed -e 's+^+https://hash-archive.org/api/enqueue/+g' | xargs -L1 curl 
 
-echo $PROV_HASHES | tr ' ' '\n' | xargs -L1 /usr/local/bin/preston cat | grep Version | cut -d' ' -f3 | sed -E $'s/^<//' | sed -E $'s/> .$//' | grep -v "deeplinker\.bio/\.well-known/genid" | sort | uniq | sed -e 's/hash:\/\/sha256/https:\/\/deeplinker.bio/g' | sed -e 's/^/https:\/\/hash-archive.org\/api\/enqueue\//g' | xargs -L1 curl 
+echo $PROV_HASHES | tr ' ' '\n' | xargs -L1 /usr/local/bin/preston cat | grep Version | cut -d' ' -f3 | sed -E $'s/^<//' | sed -E $'s/[> .]*//' | grep -v "deeplinker\.bio/\.well-known/genid" | sort | uniq | sed -e 's+hash://sha256+https://deeplinker.bio+g' | sed -e 's+^+https://hash-archive.org/api/enqueue/+g' | xargs -L1 curl 
