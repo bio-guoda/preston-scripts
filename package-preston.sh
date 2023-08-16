31a0e42711f1c0ae9df43644f1a19405ab297435
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-08-16 11:14:06 -0500

    add hashes for pubs

--- package-preston.sh
+++ package-preston.sh
@@ -87,8 +87,8 @@ References
 
 [1] ${NETWORK_LONGNAME} (${NETWORK_NAME}, ${PRESTON_NETWORK_SEED}) accessed from ${DATE_RANGE_START} to ${DATE_RANGE_END} with provenance ${LAST_PROVENANCE_VERSION}.
 [2] https://preston.guoda.bio, https://doi.org/10.5281/zenodo.1410543 . 
-[3] MJ Elliott, JH Poelen, JAB Fortes (2020). Toward Reliable Biodiversity Dataset References. Ecological Informatics. https://doi.org/10.1016/j.ecoinf.2020.101132
-[4] MJ Elliott, JH Poelen, JAB Fortes (2023). Signing data citations enables data verification and citation persistence. Scientific Data. https://doi.org/10.1038/s41597-023-02230-y
+[3] MJ Elliott, JH Poelen, JAB Fortes (2020). Toward Reliable Biodiversity Dataset References. Ecological Informatics. https://doi.org/10.1016/j.ecoinf.2020.101132 hash://sha256/136c3c1808bcf463bb04b11622bb2e7b5fba28f5be1fc258c5ea55b3b84f482c
+[4] MJ Elliott, JH Poelen, JAB Fortes (2023). Signing data citations enables data verification and citation persistence. Scientific Data. https://doi.org/10.1038/s41597-023-02230-y hash://sha256/f849c870565f608899f183ca261365dce9c9f1c5441b1c779e0db49df9c2a19d 
 
 
 
